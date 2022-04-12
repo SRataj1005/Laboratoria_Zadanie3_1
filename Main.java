30c5ff1747782f12bc8ba9bdcb183f65924362a2
Sebastian Rataj <100698720+SRataj1005@users.noreply.github.com>
2022-04-12 16:43:41 +0000

    Data urodzenia jako string

--- Main.java
+++ Main.java
@@ -1,18 +1,56 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    try {
-      Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+  public static void main(String[] args) 
+  {
+   Scanner nowy = new Scanner(System.in);
+   String imie;
+   String nazwisko;
+   int wiek;
+   String dataUrodzenia; 
+    System.out.println("Podaj wybor:");
+   int wybor = nowy.nextInt();
+    switch(wybor)
+      {
+        case 1:
+          {
+            try
+              {
+                Service1 s = new Service1();
+                nowy.nextLine();
+                System.out.println("Podaj imie: ");
+                imie = nowy.nextLine();
+                System.out.println("Podaj nazwisko: ");
+                nazwisko = nowy.nextLine();
+                System.out.println("Podaj wiek: ");
+                wiek = nowy.nextInt();
+                System.out.println("Podaj datę urodzenia: ");
+                nowy.nextLine();
+                dataUrodzenia = nowy.nextLine();
+                System.out.println("Dane zostały wprowadzone");
+                s.addStudent(new Student(imie, nazwisko, wiek, dataUrodzenia));
+              }
+              catch (IOException e) {}
+                break;
+            }
+        case 2:
+          {
+            try
+            {
+              Service1 s = new Service1();
+              var students = s.getStudents();
+              for(Student current : students)
+              {
+              System.out.println(current.ToString());
+              }
+              }
+              catch (IOException e) {}
+            }
+          }
       }
-    } catch (IOException e) {
+  
 
-    }
+
   }
-}
+  
--- Student.java
+++ Student.java
@@ -2,23 +2,31 @@ public class Student {
 
   private String Name;
   private int Age;
+  private String Surname;
+  private String DataUrodzenia;
 
-  public Student(String name, int age) {
+  public Student(String name, String surname, int age, String dataurodzenia) 
+  {
     Name = name;
     Age = age;
+    Surname = surname;
+    DataUrodzenia = dataurodzenia;
   }
 
   public String GetName() {return Name;}
+  public String GetSurname() {return Surname;}
   public int GetAge() {return Age;}
+  public String GetDataUrodzenia() {return DataUrodzenia;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return "Imię: " + Name + ", Nazwisko: " + Surname + ", Wiek: " + Integer.toString(Age) + ", Data urodzenia: " + DataUrodzenia;
+
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 4) 
+      return new Student("Parse Error", "Parse Error", -1, "Parse Error");
+    return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
   }
 }
